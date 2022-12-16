84717d94961408ce349f0061e3af707a1680d3d8
mordson <antolec2@gmail.com>
2022-12-16 14:20:33 +0100

    Jupyternotebook viewed in index.html

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route("/")
+def main():
     return render_template('index.html')
+
+@app.route('/show')
+def show():
+    return render_template('maps.html')
