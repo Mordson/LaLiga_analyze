c0ea9935c62cd35a902d98b9aba08521459ccff8
mordson <antolec2@gmail.com>
2022-12-17 17:32:56 +0100

    Added jupyternotebook to iframe, about page and github page

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ def main():
 @app.route('/show')
 def show():
     return render_template('maps.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
